0695605211505167d8bdf1b0718808dc0aac35db
maoyifei <maoyifei@magiccompass.cn>
2017-12-18 16:39:57 +0800

    爬虫小程序

---
+++ toutiao_spyder/config.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+#author by Failymao
+
+'''将收集到的相关信息存储到Mongodb中'''
+MONGO_URL = '192.168.0.187'
+MONGO_DB = 'toutiao'
+MONGO_TABLE = 'toutiao'
+
+
+GROUP_START = 1 #定义起始页
+GROUP_END = 20  #定义offset终止页
+
+KEYWORD = '街拍' #
